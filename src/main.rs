898dc93d30baaabb6ff8180f700c8bf02817b16d
farid guzman <farid.guzman91@hotmail.com>
2024-08-19 20:08:35 +0000

    refactor media impl self

--- src/main.rs
+++ src/main.rs
@@ -6,21 +6,34 @@ enum Media {
     Audiobook { title: String },
 }
 
-//rust will not let you create different selfs without their typesA
+//rust will not let you create different selfs without their types
 // description's self will be of type Media
 impl Media {
     fn description(&self) -> String {
-        //if self is of enum type Book, give access to title and author.
-        if let Media::Book { title, author} = self {
-            format!("Book: {} {}", title, author)
-            // if self is of enum type Movie, give access to title and director
-        } else if let Media::Movie { title, director } = self {
-            format!("Movie: {} {}", title, director)
-            // if self is of enum type Audiobook, give access to title
-        } else if let Media::Audiobook { title } = self {
-            format!("Audiobook: {}", title)
-        } else {
-            String::from("Media description")
+        // //if self is of enum type Book, give access to title and author.
+        // if let Media::Book { title, author} = self {
+        //     format!("Book: {} {}", title, author)
+        //     // if self is of enum type Movie, give access to title and director
+        // } else if let Media::Movie { title, director } = self {
+        //     format!("Movie: {} {}", title, director)
+        //     // if self is of enum type Audiobook, give access to title
+        // } else if let Media::Audiobook { title } = self {
+        //     format!("Audiobook: {}", title)
+        // } else {
+        //     String::from("Media description")
+        // }
+        
+        //the same can be achieved with less code using match:
+        match self {
+            Media::Book { title, author } => {
+                format!("Book: {} {}", title, author)
+            }
+            Media::Movie { title, director } => {
+                format!("Movie: {} {}", title, director)
+            }
+            Media::Audiobook { title } => {
+                format!("Audiobook: {}", title)
+            }
         }
     }
 }
@@ -37,13 +50,13 @@ fn main() {
     let audiobook = Media::Audiobook {
         title: String::from("an audiobook"),
     };
-    let good_movie = Media::Movie { 
+    let good_movie = Media::Movie {
         title: String::from("Good Movie"),
         director: String::from("Good director"),
     };
     let bad_book = Media::Book {
         title: String::from("Bad book"),
-        author: String::from("bad author")
+        author: String::from("bad author"),
     };
 
     println!("{}", audiobook.description());
@@ -53,5 +66,4 @@ fn main() {
     print_media(good_movie);
     print_media(bad_book);
     print_media(audiobook);
-
 }
